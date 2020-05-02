01176e0393d010789f09b0ebd94616cfc481acc0
Andres B <andresblasiche@gmail.com>
2020-05-01 23:53:26 -0300

    agrego chequeo de admin y GET de todas las compras

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const secret = 'asd0192AB98lipoX';
-const pathStatic = "ROOT_PATH/jwt/static/";
+const pathStatic = "/home/andres/Escritorio/FULL STACK/actividades/Market/market_turismo/static/";
 
 var app = express();
 
@@ -11,17 +11,18 @@ var usuarios = {
 		usuario: "pepe",
 		password: "cuca",
 		admin: false,
-		nivel: 1
+		nivel: 1,
+		compras: []
 	},
 	juan: {
 		usuario: "juan",
 		password: "popo",
 		admin: true,
-		nivel: 3
+		nivel: 3,
+		compras: []
 	}
 }
 
-/* MIDDLEWARES */
 
 app.get('/login', (req, res) => {
 	var user = usuarios[req.query.usuario];
@@ -30,11 +31,16 @@ app.get('/login', (req, res) => {
 		var token = jwt.sign({user: user.usuario}, secret);
 		console.log(token);
 		res.sendFile(pathStatic + "index.html");
+	} else {
+		res.status(400);
+		res.send("Usuario o contraseña incorrectos");
 	}
 });
 
+/* MIDDLEWARES */
 
 function chkLogin(req, res, next){
+	console.log("chequeo");
 	if (req.query.token) {
 		var decode = jwt.verify(req.query.token, secret);
 		console.log("Inicio: " + decode.user);
@@ -44,6 +50,26 @@ function chkLogin(req, res, next){
 	}
 }
 
+
+function chkAdmin(req, res, next){
+	var token = req.query.token;
+	if (token) {
+		var user = jwt.verify(token, secret).user;
+		if (usuarios[user].admin) {
+			req.user = user;
+			next();
+		} else {
+			res.status(400);
+			res.send("Permiso denegado");
+		}
+	} else {
+		res.status(400);
+		res.send("Es necesario un token de seguridad");
+	}
+}
+
+
+
 app.use(chkLogin);
 
 /* RUTAS */
@@ -52,6 +78,24 @@ app.get('/', (req, res) => {
 
 	res.sendFile(pathStatic + "index.html");
 });
+
+
+app.get('/compras', chkAdmin, (req, res) => {
+
+	res.json(totalCompras());
+});
+
+
+/* FUNCIONES AUXILIARES */
+
+function totalCompras(){
+	var result = [];
+	for (const us in usuarios) {
+		result.push({usuario: us, compras: usuarios[us].compras});
+	}
+	console.log(result);
+	return result;
+}
 
 
 /* SERVIDOR ESCUCHANDO */
