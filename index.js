f77a1248b18cd8f4236ecf5abbdb4a920eb50048
Andres B <andresblasiche@gmail.com>
2020-05-03 21:52:46 -0300

    correcciones

--- index.js
+++ index.js
@@ -54,50 +54,46 @@ var usuarios = {
 
 
 var paquetes = {
-	cordoba: {
+	Córdoba: {
 		destino: "Córdoba",
 		img: "cordoba.jpg",
 		descripcion: "Córdoba, la capital de la provincia argentina del mismo nombre, es conocida por su arquitectura colonial española. Alberga la Manzana Jesuítica, un complejo del siglo XVII con claustros activos, iglesias y el campus original de la Universidad Nacional de Córdoba, una de las universidades más antiguas de Sudamérica.",
-		precio: 10.000
+		precio: 10000
 	},
-	mendoza: {
+	Mendoza: {
 		destino: "Mendoza",
 		img: "mendoza.jpg",
 		descripcion: "Mendoza es una ciudad de la región de Cuyo en Argentina y es el corazón de la zona vitivinícola argentina, famosa por sus Malbecs y otros vinos tintos. Sus distintas bodegas ofrecen degustaciones y visitas guiadas.",
-		precio: 11.500
+		precio: 11500
 	},
-	bsas: {
+	BuenosAires: {
 		destino: "Buenos Aires",
 		img: "bsas2.jpg",
 		descripcion: "¿Te gustaría conocer la Ciudad de Buenos Aires y no tienes mucho tiempo? Con este recorrido podrás disfrutar del pasado y el presente de la ciudad. Podrás conocer algunos de los símbolos de la ciudad como el Obelisco, el Teatro Colón, la Plaza de Mayo y muchos más.",
-		precio: 12.000
+		precio: 12000
 	},
-	roma: {
+	Roma: {
 		destino: "Roma",
 		img: "roma.jpeg",
 		descripcion: "Roma, la capital de Italia, es una extensa ciudad cosmopolita que tiene a la vista casi 3,000 años de arte, arquitectura y cultura de influencia mundial. Las ruinas antiguas como las del Foro y el Coliseo evocan el poder del antiguo Imperio Romano.",
-		precio: 75.000
+		precio: 75000
 	},
-	paris: {
+	Paris: {
 		destino: "Paris",
 		img: "paris.jpg",
 		descripcion: "París, la capital de Francia, es una importante ciudad europea y un centro mundial del arte, la moda, la gastronomía y la cultura. Su paisaje urbano del siglo XIX está entrecruzado por amplios bulevares y el río Sena.",
-		precio: 90.000
+		precio: 90000
 	},
-	egipto: {
+	Egipto: {
 		destino: "Egipto",
 		img: "egipto.jpg",
 		descripcion: "Egipto, país que une el noreste de África con Medio Oriente, data del período de los faraones. Tiene monumentos de milenios de antigüedad ubicados junto al fértil valle del río Nilo, incluidas las colosales pirámides de Guiza y la Gran Esfinge, al igual que las tumbas del Valle de los Reyes.",
-		precio: 120.000
+		precio: 120000
 	}
 }
 
 
-
-
-
 /* MIDDLEWARES */
-
 
 function chkLogin(req, res, next){
 	console.log("chequeo");
@@ -115,7 +111,7 @@ function chkLogin(req, res, next){
 function chkUser(req, res, next){
 	var user = req.params.user;
 
-	if (!usuarios[req.user].admin || user != req.user) {
+	if (!usuarios[req.user].admin && user != req.user) {
 		res.status(400);
 		res.send("Acceso denegado");
 	}
@@ -151,18 +147,18 @@ app.get('/login', (req, res) => {
 
 
 app.post('/login', (req, res) => {
-	var { user, password } = req.body;
+	const { usuario, password } = req.body;
 
-	if (!user || !password) {
-		res.status(400);
+	if (!usuario || !password) {
+		res.status(401);
 		res.send("Usuario o contraseña incorrectos");
 	} else {
-		if (usuarios[user].password === password) {
-			var token = jwt.sign({user: user.usuario}, secret);
+		if (usuarios[usuario].password === password) {
+			var token = jwt.sign({user: usuario}, secret);
 			console.log(token);
-			res.json({usuario: user, token: token});
+			res.json({user: usuario, token: token});
 		} else {
-			res.status(400);
+			res.status(401);
 			res.send("Usuario o contraseña incorrectos");
 		}
 	}
@@ -177,9 +173,34 @@ app.get('/', (req, res) => {
 });
 
 
+app.put('/usuarios/:user', chkLogin, chkUser,(req, res) => {
+
+	if (usuarios[req.user].admin) {
+		const {nivel, admin} = req.body;
+		if (nivel) {
+			usuarios[req.params.user].nivel = nivel;
+			console.log("cambio Nivel: " + nivel);
+		}
+		if (admin != undefined) {
+			usuarios[req.params.user].admin = admin;
+			console.log("cambio Admin: " + admin);
+		}
+	} 
+	if (req.user === req.params.user) {
+		const {password} = req.body;
+		if (password) {
+			usuarios[req.user].password = password;
+			console.log("cambio Password: " + password);
+		}
+	}
+	console.log("PUT user");
+	res.json(usuarios[req.params.user]);
+});
+
+
 app.get('/usuarios', chkAdmin, (req, res) => {
-	
-	res.json(totalUsuarios);
+
+	res.json(totalUsuarios());
 });
 
 
@@ -208,22 +229,35 @@ app.get('/compras', chkAdmin, (req, res) => {
 });
 
 
+app.post('/paquetes', chkAdmin, (req, res) => {
+	const {destino, img, descripcion, precio} = req.body;
+	var id = destino.replace(/ /g,'');
+
+	paquetes[id] = {
+		destino: destino,
+		img: img,
+		descripcion: descripcion,
+		precio: precio
+	}
+	res.json(paquetes[id]);
+});
+
+
 app.get('/paquetes', chkLogin, (req, res) => {
 
 	res.json(paquetes)
 });
 
 
-app.get('/paquetes/:destino', (req, res) => {
-	var destino = req.params.destino;
-	var result = [];
+app.get('/paquetes/:destino', chkLogin, (req, res) => {
+	var destino = req.params.destino.replace(/ /g,'');
 
-	for (const paq in paquetes) {
-		if (destino === paquetes[paq].destino) {
-			result.push(paquetes[paq]);
-		}
+	if (paquetes[destino]) {
+		res.json(paquetes[destino]);
+	} else {
+		res.status(400);
+		res.send("No se encontró el paquete");
 	}
-	res.json(result);
 });
 
 
